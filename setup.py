e5bce694ca7d8545a68265eea921280404b82bde
Estelle He <wh103@duke.edu>
2017-10-31 15:20:31 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name = 'codecheck',
+	version = '1.0',
+	description = 'Checker scripts for basic_level files',
+	url = 'https://github.com/BergelsonLab/codecheck',
+	author = 'Estelle He',
+	packages = ['codecheck'])
